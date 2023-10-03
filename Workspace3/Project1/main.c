b00b4c0553a1d22616f542837d43ef70ad7ab6bb
Daniel Tremura <tremuradaniel@gmail.com>
2023-10-03 19:49:09 +0300

    creating a Default CodeLite Project Template

---
+++ Workspace3/Project1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("Default project!\n");
+	return 0;
+}
---
+++ Workspace3/Project2/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("This was may with a custom project template!\n");
+	return 0;
+}
